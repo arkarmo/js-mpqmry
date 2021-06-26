da42324d16ce2a6ca9f35559c6cc63bd4e482931
arkarmo <arkar77777cr@gmail.com>
2021-06-26 19:47:28 +0630

    1 file modified

--- index.js
+++ index.js
@@ -3,4 +3,18 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
+function app() {
+  if (true) {
+    var i= 10;
+  }
+  return i;
+}
+app();
+
+let nums=[1,2,3,4,];
+let result =nums.map(function(n){
+  return n+1;
+});
+result;
+
 
